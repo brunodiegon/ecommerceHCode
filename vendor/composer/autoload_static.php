60bd9671057ea777e85f36ce13647209290f85f7
Bruno Diego Naressi <brunodiegon@gmail.com>
2024-01-17 17:16:55 -0300

    Signed-off-by: Bruno Diego Naressi <brunodiegon@gmail.com>

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/brunodiegon/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit4ac1d95fdb7e651518457c38ec7684fa::$classMap;
 
